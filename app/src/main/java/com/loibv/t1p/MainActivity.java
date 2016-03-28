1dd5801bce3bc16401b9e876580cb5d7c1c6b8b7
Van Loi Bui <bvloi94@gmail.com>
2016-03-28 15:33:47 +0700

    add login and signup layout

--- app/src/main/java/com/loibv/t1p/MainActivity.java
+++ app/src/main/java/com/loibv/t1p/MainActivity.java
@@ -1,10 +1,15 @@
 package com.loibv.t1p;
 
-import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
+/**
+ * Created by vanloibui on 3/28/16.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,8 +17,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/SVN-Unthrift.otf");
-        TextView tvAppName = (TextView) findViewById(R.id.tvAppName);
-        tvAppName.setTypeface(custom_font);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
